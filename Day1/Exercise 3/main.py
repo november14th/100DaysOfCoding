a683e570438a7187db4735c29e0036c0742c04e7
november14th <aryalamrit7@gmail.com>
2022-12-05 21:23:55 +0545

    Day 1 commit

---
+++ Day1/Exercise 3/main.py
@@ -0,0 +1,5 @@
+# Write a program that prints the number of characters in a user's name. You might need to Google for a function that calculates the length of a string.
+
+name = input("Enter your name: ")
+length = len(name)
+print("The length of the string is " ,length)
---
+++ Day1/main.py
@@ -0,0 +1,7 @@
+print("Hello world!")
+print("First Day of Coding")
+
+print("Hello world\nHello world \nHello world\n")
+
+print("Hello" + "world")
+print("Hello" + " " + "world")
